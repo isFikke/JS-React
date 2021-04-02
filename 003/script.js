c19b16ba1174df44de8fd95ba98eaf2a18bd3b74
Fikke <ilya.f.smirnov@mail.ru>
2021-04-02 12:48:25 +0300

    Старт

---
+++ 003/script.js
@@ -0,0 +1,36 @@
+// "use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 20;
+// console.log(obj);
+
+// && и
+// || или
+
+
+let incr =  10,
+    decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr++);
+console.log(++incr);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+
+console.log(2 * 2 + 2 * 2 === 8);
+console.log(2 * 2 + 2 * 2 !== 8);
+console.log(2 * 2 + 2 * 2 != 8);
